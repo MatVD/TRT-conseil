068eef11546aeeac9c662f55f57fc6df0e6add82
MatVD <vandammemathieu@hotmail.fr>
2023-02-04 07:24:05 +0100

    home page implementation

--- assets/react/controllers/Footer.jsx
+++ assets/react/controllers/Footer.jsx
@@ -2,38 +2,42 @@ import React from "react";
 
 export default function () {
     return (
-        <footer className="md:container mx-auto flex w-full justify-between items-center sticky bottom-0">
-            <p className="text-3xl">TRT Conseil</p>
-            <div className="flex">
-                <ul className="flex-col mx-3">
-                    <li className="list-none">
-                        <a className="no-underline text-black">FAQ</a>
-                    </li>
-                    <li>
-                        <a className="no-underline text-black">Services de conseil</a>
-                    </li>
-                    <li >
-                        <a className="no-underline text-black">Conditions générales</a>
-                    </li>
-                </ul>
-                <ul className="flex-col mx-3">
-                    <li className="list-none">
-                        <a className="no-underline text-black">Qui sommes-nous ?</a>
-                    </li>
-                    <li>
-                        <a className="no-underline text-black">Nous rejoindre</a>
-                    </li>
-                    <li >
-                        <a className="no-underline text-black">Confidentialité</a>
-                    </li>
-                </ul>
+        <footer
+            className="bg-sky-800 w-full fixed bottom-0 text-white">
+            <div className="lg:container mx-auto flex w-full justify-between items-center h-40">
+                <p className="hidden text-3xl md:block">TRT Conseil</p>
+                <div className="flex">
+                    <ul className="flex-col mx-5">
+                        <li className="list-none">
+                            <a href="#" className="text-white no-underline text-black">FAQ</a>
+                        </li>
+                        <li>
+                            <a href="#" className="text-white no-underline text-black">Services de conseil</a>
+                        </li>
+                        <li>
+                            <a href="#" className="text-white no-underline text-black">Conditions générales</a>
+                        </li>
+                    </ul>
+                    <ul className="flex-col mx-3">
+                        <li className="list-none">
+                            <a href="#" className="text-white no-underline text-black">Qui sommes-nous ?</a>
+                        </li>
+                        <li>
+                            <a href="#" className="text-white no-underline text-black">Nous rejoindre</a>
+                        </li>
+                        <li>
+                            <a href="#" className="text-white no-underline text-black">Confidentialité</a>
+                        </li>
+                    </ul>
+                </div>
+                <button type="button" className="hidden shadow-none p-2 border-2 rounded-2xl md:block">Nous contacter</button>
+                <div className="flex mr-4">
+                    <i className="fa-brands fa-twitter fa-xl"></i>
+                    <i className="fa-brands fa-facebook fa-xl ml-2"></i>
+                    <i className="fa-brands fa-linkedin fa-xl ml-2"></i>
+                </div>
             </div>
-            <button type="button" className="shadow-none p-2 border-2 rounded-2xl">Nous contacter</button>
-            <div className="flex">
-                <i className="fa-brands fa-twitter fa-xl"></i>
-                <i className="fa-brands fa-facebook fa-xl ml-2"></i>
-                <i className="fa-brands fa-linkedin fa-xl ml-2"></i>
-            </div>
+
         </footer>
     )
 }
--- assets/react/controllers/Navbar.jsx
+++ assets/react/controllers/Navbar.jsx
@@ -2,23 +2,23 @@ import React from "react";
 
 export default function () {
     return (
-        <header className="container mx-auto flex bg-amber-400 w-full h-35">
-            <nav className="md:container flex justify-between items-center">
+        <header className="bg-sky-800 h-35 w-full">
+            <nav className="lg:container mx-auto flex justify-between items-center text-white">
                 <div
-                    className="group flex h-20 w-20 cursor-pointer items-center justify-center bg-amber-400">
+                    className="group flex h-20 w-20 cursor-pointer items-center justify-center bg-sky-800">
                     <div className="space-y-2">
                             <span
                                 className="block h-1 w-10 origin-center rounded-full bg-slate-500 transition-transform ease-in-out group-hover:translate-y-1.5 group-hover:rotate-45"></span>
                         <span
                             className="block h-1 w-8 origin-center rounded-full bg-orange-500 transition-transform ease-in-out group-hover:w-10 group-hover:-translate-y-1.5 group-hover:-rotate-45"></span>
                         <span
-                            className="block h-1 w-8 origin-center rounded-full bg-gray-500 transition-transform ease-in-out group-hover:hidden"></span>
+                            className="block h-1 w-8 origin-center rounded-full bg-white transition-transform ease-in-out group-hover:hidden"></span>
                     </div>
                 </div>
-                <p className="text-3xl">TRT Conseil</p>
+                <p className="hidden text-3xl md:block">TRT Conseil</p>
                 <div className="flex items-center">
                     <i className="fa-solid fa-user fa-xl"></i>
-                    <p className="ml-4">Mon compte</p>
+                    <p className="ml-4 mr-4">Compte</p>
                 </div>
             </nav>
         </header>
